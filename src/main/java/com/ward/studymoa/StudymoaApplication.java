b8cf6b7858ee7d381a52a94eefdc00769dceef0a
leafy <icarus8050@naver.com>
2019-06-22 04:00:43 +0900

    project init

---
+++ src/main/java/com/ward/studymoa/StudymoaApplication.java
@@ -0,0 +1,13 @@
+package com.ward.studymoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudymoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudymoaApplication.class, args);
+	}
+
+}
