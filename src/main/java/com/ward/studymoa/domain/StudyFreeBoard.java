0649d7c11b79b0350a20d385663c439f9949552e
leafy <icarus8050@naver.com>
2019-06-23 01:57:47 +0900

    init

---
+++ src/main/java/com/ward/studymoa/domain/StudyFreeBoard.java
@@ -0,0 +1,44 @@
+/**
+ * StudyFreeBoard.class
+ *
+ * Copyright (c) 2019 WARD.
+ */
+package com.ward.studymoa.domain;
+
+import com.ward.studymoa.domain.audit.BoardBaseEntity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+/**
+ * StudyUser가 작성한 잡담 글에 대한 Entity
+ *
+ * @author Chul Yun
+ * @since 2019. 06. 23
+ * @version 1.00
+ */
+@Entity
+@NoArgsConstructor
+@Getter
+public class StudyFreeBoard extends BoardBaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idx;
+
+    @ManyToOne
+    @JoinColumn(name = "user_idx", nullable = false, insertable = false, updatable = false)
+    private StudyUser studyUser;
+
+    @Column(length = 300, nullable = false)
+    private String freeBoardTitle;
+
+    @Column(columnDefinition = "TEXT")
+    private String freeBoardDesc;
+
+    @Override
+    public void setCreatedBy() {
+        this.createdBy = studyUser.getId();
+    }
+}
---
+++ src/main/java/com/ward/studymoa/domain/StudyWanted.java
@@ -0,0 +1,44 @@
+/**
+ * StudyWanted.class
+ *
+ * Copyright (c) 2019 WARD.
+ */
+package com.ward.studymoa.domain;
+
+import com.ward.studymoa.domain.audit.BoardBaseEntity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+/**
+ * StudyUser가 스터디를 구하는 글에 대한 Entity
+ *
+ * @author Chul Yun
+ * @since 2019. 06. 23
+ * @version 1.00
+ */
+@Entity
+@NoArgsConstructor
+@Getter
+public class StudyWanted extends BoardBaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idx;
+
+    @ManyToOne
+    @JoinColumn(name = "user_idx", updatable = false, insertable = false, nullable = false)
+    private StudyUser studyUser;
+
+    @Column(nullable = false, length = 300)
+    private String wandtedTitle;
+
+    @Column(columnDefinition = "TEXT")
+    private String wandtedDesc;
+
+    @Override
+    public void setCreatedBy() {
+        this.createdBy = studyUser.getId();
+    }
+}
