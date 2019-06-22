2e89fc54a98a334f24faeb7c9c50a72479353a2b
leafy <icarus8050@naver.com>
2019-06-23 01:57:32 +0900

    createdBy 필드의 길이 재설정

--- src/main/java/com/ward/studymoa/domain/audit/BoardBaseEntity.java
+++ src/main/java/com/ward/studymoa/domain/audit/BoardBaseEntity.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import javax.persistence.Column;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 
@@ -14,5 +15,8 @@ import javax.persistence.MappedSuperclass;
 public abstract class BoardBaseEntity extends BaseEntity {
 
     @CreatedBy
+    @Column(nullable = false, length = 20)
     protected String createdBy;
+
+    public abstract void setCreatedBy();
 }
