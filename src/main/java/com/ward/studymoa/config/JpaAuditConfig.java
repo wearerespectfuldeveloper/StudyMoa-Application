dcea2593fc3da374b3560ccd4ac4ab8e82e00073
leafy <icarus8050@naver.com>
2019-06-23 15:23:33 +0900

    컨벤션 주석 추가

--- src/main/java/com/ward/studymoa/config/JpaAuditConfig.java
+++ src/main/java/com/ward/studymoa/config/JpaAuditConfig.java
@@ -1,8 +1,20 @@
+/**
+ * JpaAuditConfig.class
+ *
+ * Copyright (c) 2019 WARD.
+ */
 package com.ward.studymoa.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+/**
+ * Entity의 작성일, 수정일 등 정보를 Auditing 하기 위한 설정 클래스
+ *
+ * @author Chul Yun
+ * @since 2019. 06. 22
+ * @version 1.00
+ */
 @Configuration
 @EnableJpaAuditing
 public class JpaAuditConfig {
