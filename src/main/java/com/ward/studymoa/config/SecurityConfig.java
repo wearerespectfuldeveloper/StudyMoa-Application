70bb955c655003891c1d276a8325543c95dbc269
leafy <icarus8050@naver.com>
2019-06-25 22:52:24 +0900

    간단한 테스트를 위해 시큐리티 설정

--- src/main/java/com/ward/studymoa/config/SecurityConfig.java
+++ src/main/java/com/ward/studymoa/config/SecurityConfig.java
@@ -3,8 +3,21 @@ package com.ward.studymoa.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/")
+                .permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .addFilterBefore(new CharacterEncodingFilter(), CsrfFilter.class)
+                .csrf().disable();
+    }
 }
