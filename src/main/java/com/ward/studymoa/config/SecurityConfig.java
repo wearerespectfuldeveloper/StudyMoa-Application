e0f8811c3f74f2e58a97a2c8cfbce2c672150646
leafy <icarus8050@naver.com>
2019-06-25 23:02:19 +0900

    간단 정리

--- src/main/java/com/ward/studymoa/config/SecurityConfig.java
+++ src/main/java/com/ward/studymoa/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.ward.studymoa.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.csrf.CsrfFilter;
