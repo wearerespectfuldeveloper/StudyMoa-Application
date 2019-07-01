4e8081fb11b5236e9791311d6af6734c86149015
leafy <icarus8050@naver.com>
2019-07-01 23:20:13 +0900

    운영체제 환경별 private-setting.yml path 설정 변경 코드 작성

--- src/main/java/com/ward/studymoa/EnvironmentPostProcessorLoad.java
+++ src/main/java/com/ward/studymoa/EnvironmentPostProcessorLoad.java
@@ -15,6 +15,8 @@ import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 외부 설정 파일을 로드하는 클래스
@@ -25,10 +27,13 @@ import java.io.IOException;
 public class EnvironmentPostProcessorLoad implements EnvironmentPostProcessor {
 
     private final YamlPropertySourceLoader loader = new YamlPropertySourceLoader();
+    private final String WINDOW_PATH = "C:/private-setting.yml";
+    private final String MAX_PATH = "/Users/private-setting.yml";
+    private final String LINUX_PATH = "/home/ec2-user/private-setting.yml";
 
     @Override
     public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
-        Resource path = new FileSystemResource("C:/private-setting.yml");
+        Resource path = new FileSystemResource(getPrivateResourcePath());
         PropertySource<?> propertySource = loadYaml(path);
         environment.getPropertySources().addLast(propertySource);
     }
@@ -42,6 +47,18 @@ public class EnvironmentPostProcessorLoad implements EnvironmentPostProcessor {
         } catch (IOException ex) {
             throw new IllegalStateException(
                     "Failed to load yaml configuration from " + path, ex);
+        }
+    }
+
+    private String getPrivateResourcePath() {
+        String osName = System.getProperty("os.name").toLowerCase();
+
+        if (osName.contains("win")) {
+            return WINDOW_PATH;
+        } else if (osName.contains("mac")) {
+            return MAX_PATH;
+        } else {
+            return LINUX_PATH;
         }
     }
 }
