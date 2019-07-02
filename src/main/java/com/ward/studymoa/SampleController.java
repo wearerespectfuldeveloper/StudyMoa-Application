d7bec66a6800541cc198169d5dad7636a74622b3
leafy <icarus8050@naver.com>
2019-07-02 22:52:17 +0900

    테스트 컨트롤러

---
+++ src/main/java/com/ward/studymoa/SampleController.java
@@ -0,0 +1,15 @@
+package com.ward.studymoa;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+
+    @GetMapping(name = "/")
+    @ResponseBody
+    public String sampleRequest() {
+        return "Hello StudyMoa!";
+    }
+}
