5faefc01af2b700639e23a1ac11e1c3dc9b76680
icarus8050 <icarus8050@naver.com>
2019-07-08 23:28:26 +0900

    배너 추가

--- src/test/java/com/ward/studymoa/StudymoaApplicationTests.java
+++ src/test/java/com/ward/studymoa/StudymoaApplicationTests.java
@@ -1,13 +1,19 @@
 package com.ward.studymoa;
 
+import com.ward.studymoa.api.service.StudyCategoryService;
+import com.ward.studymoa.core.StudyCategory;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class StudymoaApplicationTests {
+
+	@Autowired
+	StudyCategoryService studyCategoryService;
 
 	@Test
 	public void contextLoads() {
