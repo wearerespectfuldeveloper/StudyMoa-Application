492d2e63872f3e905fb92f03c65f3c3a95fcc071
icarus8050 <icarus8050@naver.com>
2019-07-14 23:44:05 +0900

    OAuth2 적용이 시간이 걸리므로 일단 앱이 돌아갈 수 있게 수정 (OAuth2 연동은 차후에..)

--- src/main/java/com/ward/studymoa/common/config/SecurityConfig.java
+++ src/main/java/com/ward/studymoa/common/config/SecurityConfig.java
@@ -1,25 +1,31 @@
 package com.ward.studymoa.common.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
+import org.springframework.security.oauth2.client.InMemoryOAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.csrf.CsrfFilter;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
 import javax.servlet.Filter;
 
+import static com.ward.studymoa.common.state.SocialType.GOOGLE;
+import static com.ward.studymoa.common.state.SocialType.NAVER;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    private final Filter ssoFilter;
-
-    public SecurityConfig(Filter ssoFilter) {
-        this.ssoFilter = ssoFilter;
-    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -29,12 +35,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         characterEncodingFilter.setForceEncoding(true);
 
         http.authorizeRequests()
-                .antMatchers("/", "/login")
+                .antMatchers("/", "/oauth2/**", "/login/**", "/css/**",
+                        "/images/**", "/js/**", "/console/**")
                 .permitAll()
                 .anyRequest().authenticated()
-                .and().headers().frameOptions().sameOrigin()
-                .and().csrf().disable()
-                .addFilterBefore(characterEncodingFilter, CsrfFilter.class)
-                .addFilterBefore(ssoFilter, BasicAuthenticationFilter.class);
+                .and()
+                .csrf().disable()
+                .addFilterBefore(characterEncodingFilter, CsrfFilter.class);
     }
 }
