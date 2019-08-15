a18af739bec8e88dae56a0632aa1b65a1c54c55f
icarus8050 <icarus8050@naver.com>
2019-08-15 20:35:49 +0900

    @NonNull 애너테이션으로 변경

--- src/main/java/com/ward/studymoa/common/config/AuditorAwareImpl.java
+++ src/main/java/com/ward/studymoa/common/config/AuditorAwareImpl.java
@@ -1,15 +1,15 @@
 package com.ward.studymoa.common.config;
 
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.lang.NonNull;
 import org.springframework.security.core.context.SecurityContextHolder;
 
-import javax.annotation.Nonnull;
 import java.util.Optional;
 
 public class AuditorAwareImpl implements AuditorAware<String> {
 
     @Override
-    @Nonnull
+    @NonNull
     public Optional<String> getCurrentAuditor() {
         return Optional.of(SecurityContextHolder.getContext().getAuthentication().getName());
     }
